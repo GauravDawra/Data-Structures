4139221ded3eb51254b1a3924d3c81ffb51667af
GauravDawra <55682270+GauravDawra@users.noreply.github.com>
2020-05-08 23:41:34 +0530

    C++ template

---
+++ g.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<cstring>
+#include<unordered_map>
+#include<math.h>
+#include<map>
+#include<algorithm>
+#include<iomanip>
+#include<stdio.h>
+#include<set>
+#include<queue>
+#include<tuple>
+// #include<bits/stdc++.h>
+// #include "unionFind.h"
+using namespace std;
+#define ll long long
+typedef vector<ll> vi;
+typedef pair<ll, ll>  ii;
+typedef vector<vi> vvi;
+#define INF 1e9
+#define INF64 (ll)1e18
+#define pb push_back
+#define F first
+#define S second
+#define MP make_pair
+#define forn(i,l,r) for(ll i=l ; i<r ; i++)
+
+ll max(ll a, ll b) {return (a>b)?a:b;}
+ll min(ll a, ll b) {return (a>b)?b:a;}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
